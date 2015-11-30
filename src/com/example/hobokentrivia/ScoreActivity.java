a335a4aa93bf3050389852045f4e4f6f789ab08d
Somayah88 <Somayah88@users.noreply.github.com>
2015-11-29 21:10:04 -0500

    Update ScoreActivity.java

--- src/com/example/hobokentrivia/ScoreActivity.java
+++ src/com/example/hobokentrivia/ScoreActivity.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
@@ -14,7 +15,6 @@ import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
